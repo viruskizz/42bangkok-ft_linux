e778ff36f735b00eb5bfc1476ad36b07b80df3d5
viruskizz <kizzaraiva@gmail.com>
2025-01-08 14:47:30 +0700

    saved

--- scripts/compling.sh
+++ scripts/compling.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+# Setup environment variables
+LFS=/mnt/lfs
+
+#########################################
+# 5.4.1. Installation of Cross Binutils #
+#########################################
+tar -xpvf binutils-2.32.tar.xz
+cd binutils-2.32
+mkdir -vp build
+cd build
+
+../configure --prefix=/tools            \
+             --with-sysroot=$LFS        \
+             --with-lib-path=/tools/lib \
+             --target=$LFS_TGT          \
+             --disable-nls              \
+             --disable-werror
+
+make
+
+case $(uname -m) in
+  x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
+esac
+
+make install
+
+cd ../..
+
+rm -rf binutils-2.32
+rm binutils-2.32.tar.xz
